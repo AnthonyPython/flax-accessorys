7fa877d1c5a9e6ce89e9137691001eec1f093eb5
Anthonypython <hensonstephen61@gmail.com>
2021-09-04 19:01:50 -0400

    Added shaking script

---
+++ shakeobject/ShakeScript.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using FlaxEngine;
+using FlaxEngine.Utilities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    /// <summary>
+    /// ShakeScript Script.
+    /// </summary>
+    public class ShakeScript : Script
+    {
+        /// <inheritdoc/>
+        /// 
+        Transform _target;
+        Vector3 _initialPos;
+
+        float _pendingShakeDuration = 0f;
+
+        bool isShaking = false;
+
+        private Task _task;
+
+        public float intensity = 1f;
+        public override void OnStart()
+        {
+            _target = Actor.Transform;
+            _initialPos = Actor.LocalPosition;
+            Debug.Log("Camera Z position: " + _initialPos.Z.ToString());
+        }
+
+        public void Shake(float duration, float strength)
+        {
+            if (duration > 0)
+            {
+                intensity = strength;
+                _pendingShakeDuration += duration;
+            }
+        }
+        
+        /// <inheritdoc/>
+        public override void OnEnable()
+        {
+            _task = Task.Run(DoShake);
+        }
+
+        /// <inheritdoc/>
+        public override void OnDisable()
+        {
+            // End async work
+            _task.Wait();
+        }
+
+        /// <inheritdoc/>
+        public override void OnUpdate()
+        {
+            if (_pendingShakeDuration > 0 && !isShaking)
+            {
+
+                _task = Task.Run(AsyncShake);
+            }
+            
+        }
+
+        async Task AsyncShake()
+        {
+            var startTime = Time.TimeSinceStartup;
+            while (Time.TimeSinceStartup < startTime + _pendingShakeDuration)
+            {
+                DoShake().MoveNext();
+            }
+            _pendingShakeDuration = 0f;
+            Actor.LocalPosition = _initialPos;
+            isShaking = false;
+        }
+
+       IEnumerator DoShake()
+        {
+            isShaking = true;
+            //Debug.Log("Shake started");
+            var startTime = Time.TimeSinceStartup;
+            while (Time.TimeSinceStartup < startTime + _pendingShakeDuration)
+            {
+               // Debug.Log("Shake");
+                var rand = new Random();
+                var randomPoint = new Vector3( ( (float)rand.NextDouble(-1f, 1f) * intensity)+ _initialPos.X,  ( (float)rand.NextDouble(-1f, 1f) * intensity) + _initialPos.Y, _initialPos.Z);
+
+                Debug.Log("Camera Z position: " + randomPoint.Z.ToString());
+                Actor.LocalPosition = randomPoint;
+                yield return null;
+            }
+            
+
+            _pendingShakeDuration = 0f;
+            Actor.LocalPosition = _initialPos;
+            isShaking = false;
+
+            
+        }
+    }
+}
---
+++ shakeobject/ShakeTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+
+namespace Game
+{
+    /// <summary>
+    /// ShakeTest Script.
+    /// </summary>
+    /// 
+
+    public class ShakeTest : Script
+    {
+        public ShakeScript Shaker;
+        public float duration = 1f;
+        public float strength = 1f;
+        /// <inheritdoc/>
+        public override void OnStart()
+        {
+            // Here you can add code that needs to be called when script is created, just before the first game update
+        }
+        
+        /// <inheritdoc/>
+        public override void OnEnable()
+        {
+            // Here you can add code that needs to be called when script is enabled (eg. register for events)
+        }
+
+        /// <inheritdoc/>
+        public override void OnDisable()
+        {
+            // Here you can add code that needs to be called when script is disabled (eg. unregister from events)
+        }
+
+        /// <inheritdoc/>
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyUp(KeyboardKeys.T))
+            {
+                Shaker.Shake(duration, strength);
+            }
+        }
+    }
+}
